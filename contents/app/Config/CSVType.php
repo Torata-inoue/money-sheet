469f9669d245bfcb76784a752b888f860d58f308
Torata-Inoue <i_am_torao@yahoo.co.jp>
2023-12-28 08:57:29 +0900

    RakutenCard終わり

--- contents/app/Config/CSVType.php
+++ contents/app/Config/CSVType.php
@@ -4,20 +4,21 @@ namespace App\Config;
 
 use App\Domains\History\HistoryInterface;
 use App\Domains\History\RakutenBankHistory;
+use App\Domains\History\RakutenCardHistory;
 
 enum CSVType
 {
 //    case UFJ;
-    case RAKUTEN_BANK;
-//    case RAKUTEN_MINE;
+//    case RAKUTEN_BANK;
+    case RAKUTEN_MINE;
 //    case RAKUTEN_SHARE;
 
     public function getCharacterCode(): string
     {
         return match ($this) {
 //            self::UFJ => 'Shift-JIS',
-            self::RAKUTEN_BANK => 'Shift-JIS',
-//            self::RAKUTEN_MINE => 'UTF-8',
+//            self::RAKUTEN_BANK => 'Shift-JIS',
+            self::RAKUTEN_MINE => 'UTF-8',
 //            self::RAKUTEN_SHARE => 'UTF-8',
         };
     }
@@ -26,8 +27,8 @@ enum CSVType
     {
         return match ($this) {
 //            self::UFJ => 'ufj.csv',
-            self::RAKUTEN_BANK => 'rakuten_bank.csv',
-//            self::RAKUTEN_MINE => 'rakuten_7271.csv',
+//            self::RAKUTEN_BANK => 'rakuten_bank.csv',
+            self::RAKUTEN_MINE => 'rakuten_7271.csv',
 //            self::RAKUTEN_SHARE => 'rakuten_1560.csv',
         };
     }
@@ -39,9 +40,19 @@ enum CSVType
     {
         return match ($this) {
 //            self::UFJ => 'ufj.csv',
-            self::RAKUTEN_BANK => RakutenBankHistory::class,
-//            self::RAKUTEN_MINE => 'rakuten_7271.csv',
-//            self::RAKUTEN_SHARE => 'rakuten_1560.csv',
+//            self::RAKUTEN_BANK => RakutenBankHistory::class,
+            self::RAKUTEN_MINE => RakutenCardHistory::class,
+//            self::RAKUTEN_SHARE => RakutenCardHistory::class,
+        };
+    }
+
+    public function getCardName(): string
+    {
+        return match ($this) {
+//            self::UFJ => 'ufj.csv',
+            self::RAKUTEN_BANK => 'Rakuten デビットMaster',
+            self::RAKUTEN_MINE => 'Rakuten Visa 7271',
+            self::RAKUTEN_SHARE => 'Rakuten Visa 1560',
         };
     }
 }
---
+++ contents/app/Domains/History/RakutenCardHistory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Domains\History;
+
+use Carbon\Carbon;
+
+class RakutenCardHistory implements HistoryInterface
+{
+    const TRADING_DAY = 0;
+    const CONTENT = 1;
+    const MONEY = 4;
+
+    private string $trading_day;
+    private string $money;
+    private string $content;
+
+    public function __construct(array $row)
+    {
+        $this->trading_day = $row[self::TRADING_DAY];
+        $this->money = $row[self::MONEY];
+        $this->content = $row[self::CONTENT];
+    }
+
+    public function getTradingDate(): Carbon
+    {
+        return Carbon::createFromFormat('Y/m/d', $this->getTradingDate());
+    }
+
+    public function getClassification(): string
+    {
+        return '出金';
+    }
+
+    public function getMoney(): int
+    {
+        return (int) $this->money;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function isDescribe(): bool
+    {
+        return true;
+    }
+
+    public static function skipRow(array $row): bool
+    {
+        return empty($row[0]);
+    }
+}
